c19b79a5616cf3ec481d3c466d7ec3273108537e
unknown <niteshqatester4@gmail.com>
2026-02-23 20:25:49 +0530

    democode

---
+++ src/test/java/com/Ec_project/login/LoginClass.java
@@ -0,0 +1,44 @@
+package com.Ec_project.login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class LoginClass 
+{
+	
+
+	public static void main(String[] args)
+	{
+		//driver integration
+		//System.setProperty("webdriver.chrome.driver","C:\\Users\\DELL\\Desktop\\Demo\\Ec_project\\drivers\\chromedriver.exe");
+		
+		WebDriver driver=new EdgeDriver();//open chrome browser
+		driver.get("https://www.saucedemo.com/");
+	
+		WebElement username=driver.findElement(By.name("user-name"));//loc+f
+		username.sendKeys("standard_user");//actions
+		
+		
+		WebElement password=driver.findElement(By.id("password"));
+		
+		password.sendKeys("secret_sauce");
+		
+		
+		WebElement submit=driver.findElement(By.className("submit-button"));
+		
+		submit.click();
+		
+		WebElement link =driver.findElement(By.partialLinkText("Sauce Labs Backpa"));
+		link.click();
+
+	}
+
+}
+//By.id()
+//By.name();
+//By.className();
+//By.linkText();
+//By.partialLinkText();
